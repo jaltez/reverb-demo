271c504ea300ca436f47a75eb7fa99141f1b55de
Javier Altez <jaltez1@gmail.com>
2024-03-20 00:29:12 +0100

    Initial interaction

--- app/Livewire/HomeComponent.php
+++ app/Livewire/HomeComponent.php
@@ -2,27 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Events\UserClicked;
 use App\Events\UserConnected;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class HomeComponent extends Component
 {
-    public array $logs = ['Logs'];
+    public array $actions = [];
+
+    public array $events = [];
 
     public function mount()
     {
-        UserConnected::dispatch('User Connected');
+        UserConnected::dispatch();
     }
 
-    #[On('echo:everyone,UserConnected')]
-    public function userConnectedEvent()
+    public function buttonClick()
     {
-        $this->logs[] = 'User Connected Event';
+        UserClicked::dispatch();
+        $this->actions[] = 'click';
     }
 
     public function render()
     {
         return view('livewire.home');
+    }
+
+    #[On('echo:everyone,.user.connected')]
+    public function userConnectedEvent()
+    {
+        $this->events[] = 'UserConnected';
+    }
+
+    #[On('echo:everyone,.user.clicked')]
+    public function userClickedEvent()
+    {
+        $this->events[] = 'UserClicked';
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,13 +1,33 @@
-<div wire:click=''>
-    <h1>REVERB DEMO</h1>
-    {{-- <button @click="$dispatch('UserConnected')">Button</button> --}}
+<div class="m-8">
+    <h1 class="text-2xl font-bold text-slate-700 mb-4">REVERB DEMO</h1>
+
+    <button class="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded" wire:click="buttonClick">
+        Click
+    </button>
+
     <div>
-        <ul>
-            @foreach ($logs as $log)
-                <li>
-                    {{ $log }}
-                </li>
-            @endforeach
-        </ul>
+        <div class="grid grid-cols-2 gap-4">
+            <div class="my-5 p-4 border border-slate-300 rounded-md">
+                <h2 class="text-xl font-bold text-slate-700 mb-4">Actions:</h2>
+                <ul class="list-disc list-inside">
+                    @foreach ($actions as $action)
+                        <li>
+                            {{ $action }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="my-5 p-4 border border-slate-300 rounded-md">
+                <h2 class="text-xl font-bold text-slate-700 mb-4">Events:</h2>
+                <ul class="list-disc list-inside">
+                    @foreach ($events as $event)
+                        <li>
+                            {{ $event }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
